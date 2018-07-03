b1af07d12b4071a031b4bb58b9f15ab4886abc05
muhammed-noufal <muhammed.noufal@citrusinformatics.com>
2018-07-03 18:18:06 +0530

    Add design in Login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,8 +10,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  authForm: FormGroup;
+  validation_messages = {
+    'email': [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'minlength', message: 'Email must be at least 5 characters long.' },
+      // { type: 'maxlength', message: 'Email cannot be more than 25 characters long.' },
+      { type: 'pattern', message: 'Your Email must match valid email format.' },
+      // { type: 'validEmail', message: 'Your Email has already been taken.' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required.' },
+      { type: 'minlength', message: 'Password must be at least 6 characters long.' },
+      { type: 'maxlength', message: 'Password cannot be more than 25 characters long.' }
+    ]
   }
+
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off';
+
+  hideShowPassword() {
+    this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+    this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+  }
+
+
+  constructor(public navCtrl: NavController,
+    private formBuilder: FormBuilder) {
+    this.authForm = formBuilder.group({
+      email: ['', Validators.compose(
+        [Validators.required,
+        Validators.minLength(5),
+        // Validators.maxLength(25),
+        Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    });
+  }
+
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
