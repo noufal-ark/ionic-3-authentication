2db09464eacce22dcf1f2f942fea6afe0728e21f
muhammed-noufal <muhammed.noufal@citrusinformatics.com>
2018-07-04 12:50:45 +0530

    Add design on registration page.

--- src/message/error.message.ts
+++ src/message/error.message.ts
@@ -10,5 +10,21 @@ export const validation_messages = {
         { type: 'required', message: 'Password is required.' },
         { type: 'minlength', message: 'Password must be at least 6 characters long.' },
         { type: 'maxlength', message: 'Password cannot be more than 25 characters long.' }
+    ],
+    'confirmpassword': [
+        { type: 'required', message: 'Password is required.' },
+        { type: 'minlength', message: 'Password must be at least 6 characters long.' },
+        { type: 'maxlength', message: 'Password cannot be more than 25 characters long.' }
+    ],
+    'personname': [
+        { type: 'required', message: 'Name is required.' },
+        { type: 'minlength', message: 'Name must be at least 3 characters long.' },
+        { type: 'maxlength', message: 'Name cannot be more than 25 characters long.' },
+        { type: 'pattern', message: 'Your name only allow charecters.' },
+    ],
+    'mobile': [
+        { type: 'required', message: 'Mobile number is required.' },
+        { type: 'minlength', message: 'Mobile number must be at least 10 characters long.' }
     ]
 }
+export const PASSWORD_UNMATCH = 'Password must match with confirm password.';
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,12 +1,7 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { validation_messages, PASSWORD_UNMATCH } from './../../message/error.message';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the RegisterPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,12 +9,90 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'register.html',
 })
 export class RegisterPage {
+  authRegisterForm: FormGroup;
+  validation_messages = validation_messages;
+  PASSWORD_UNMATCH = PASSWORD_UNMATCH;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off';
+
+  placeholderMobile = '';
+
+  hideShowPassword() {
+    this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+    this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+  }
+
+
+  constructor(public navCtrl: NavController,
+    private formBuilder: FormBuilder) {
+    this.authRegisterForm = formBuilder.group({
+      email: ['', Validators.compose(
+        [Validators.required,
+        Validators.minLength(5),
+        // Validators.maxLength(25),
+        Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)])],
+      password: ['', Validators.compose(
+        [Validators.required,
+        Validators.minLength(6)])],
+      confirmPassword: ['', Validators.compose(
+        [Validators.required,
+        Validators.minLength(6)])],
+      personName: ['', Validators.compose(
+        [Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(64),
+        Validators.pattern(/^[a-zA-Z ]{2,30}$/)])],
+      mobile: ['', Validators.compose(
+        [Validators.required,
+        Validators.minLength(14)])],
+    }, { validator: this.matchingPasswords('password', 'confirmPassword') });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
   }
 
+
+  /**
+   * To make standard telecommunication numbering based on US. 
+   * @param input key input
+   */
+  formatMobileNumber(input) {
+    setTimeout(() => {
+      var x = this.authRegisterForm.controls.mobile.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+      this.authRegisterForm.controls.mobile.setValue(!x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : ''));
+      input.selectionStart = input.selectionEnd = 1000;
+    }, 0);
+    input.selectionStart = input.selectionEnd = 1000;
+  }
+  /**
+   * This method used to change placeholder value when input get focus.
+   */
+  onMobileFocus() {
+    this.placeholderMobile = '(555) 555-5555';
+  }
+  /**
+   * This method used to change placeholder value when input get focus out.
+   */
+  onMobileFocusOut() {
+    this.placeholderMobile = '';
+  }
+
+  /**
+   * To match password and return boolean.
+   * @param passwordKey password value
+   * @param confirmPasswordKey confirm password value
+   */
+  matchingPasswords(passwordKey: string, confirmPasswordKey: string) {
+    return (group: FormGroup): { [key: string]: any } => {
+      let password = group.controls[passwordKey];
+      let confirmPassword = group.controls[confirmPasswordKey];
+      if (password.value !== confirmPassword.value) {
+        return {
+          mismatchedPasswords: true
+        };
+      }
+    }
+  }
 }
